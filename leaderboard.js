4649090a8e1a351a7c1c3ad4391d4a12854659d3
vinaypuppal <puppalvinay@gmail.com>
2015-07-17 04:35:23 +0530

    complete backend part

--- leaderboard.js
+++ leaderboard.js
@@ -1,11 +1,51 @@
 PlayersList = new Mongo.Collection('playerslist');
 if (Meteor.isClient) {
   Template.LeaderBoard.helpers({
-    
+    players:function(){
+      return PlayersList.find({},{sort:{score:-1,name:1}});
+    },
+    selectedPlayer:function(){
+      var playerId=this._id;
+      var selectedId = Session.get('selectedPlayer');
+      return playerId === selectedId ? 'selected' : '';
+    },
+    selectedPlayerName:function(){
+      var selectedId = Session.get('selectedPlayer');
+      return selectedId ? PlayersList.findOne({_id:selectedId}).name : '';
+    }
   });
 
   Template.LeaderBoard.events({
-    
+    'click .player': function (e,t) {
+        var playerId = this._id;
+        Session.set('selectedPlayer', playerId);
+        console.log(playerId);
+    }
+  });
+
+  Template.AddPlayer.events({
+    'submit form[name=addPlayer]':function(e,t){
+      e.preventDefault();
+      var playerName=t.find('input[type=text]').value;
+      if(playerName !== ''){
+        PlayersList.insert({
+          name:playerName,
+          score:0,
+          createdAt:new Date
+        });
+      }
+      t.find('form[name=addPlayer]').reset();
+    }
+  });
+  Template.ScoreButtons.events({
+    'click .increment': function (e,t) {
+      var selectedId = Session.get('selectedPlayer');
+      PlayersList.update({_id:selectedId}, {$inc:{score:5}});
+    },
+    'click .decrement': function (e,t) {
+      var selectedId = Session.get('selectedPlayer');
+      PlayersList.update({_id:selectedId}, {$inc:{score:-5}});
+    }
   });
 }
 
